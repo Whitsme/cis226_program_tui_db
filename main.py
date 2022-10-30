f461ad152a03d53483f246e08ba5dbfb23a768b3
Aaron <95139987+Whitsme@users.noreply.github.com>
2022-10-30 17:35:45 -0400

    sudo animation added
    sudo animation added. still need to learn how to actually animate objects in textual. computation is pending. I ended up pushing forward on the animation as the compute was easy to transfer over, but i still need to add it back in again and this time link it to the corresponding buttons -> text fields

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
+from signal import pause
 from tkinter import Widget
+from turtle import delay
 from textual.app import App, ComposeResult
 from textual.color import Color
 from textual.containers import Container, Horizontal, Vertical, Grid
 from textual.message import Message, MessageTarget
-from textual.reactive import reactive
+from textual.reactive import reactive, var
 from textual.screen import Screen
 from textual.timer import Timer
 from textual.widgets import Static, Button, Header, Footer, Input
 from textual import events
 
+import sqlite3
+import time
+
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+error = ["error"]
+warning = ["warning"]
+confirm = ["confirm"]
+i = [0]
+INSERT: object = Button("Insert", id="insert")
+SELECT: object = Button("Select", id="select")
+CREATE: object = Button("Create", id="create")
+ERROR: object = Static(error[0], id="error")
+WARNING: object = Static(warning[0], id="warning")
+CONFIRM: object = Static(confirm[0], id="confirm")
+SUBMIT: object = Button("Submit", id="submit")
+
 class TuiGrid(App):
-    CSS_PATH = "grid0.css"
+    CSS_PATH = "grid.css"
 
     def compose(self) -> ComposeResult:
-        input_error = "Quantity:\ncontains non-integers"
-
         yield Container(
                 Horizontal(
                     Static("Vegetable Stand"),
@@ -22,21 +39,18 @@ class TuiGrid(App):
                 ),
                 Container(
                     Container(
-                        Button("Insert", id="insert"),
-                        Button("Select", id="select"),
-                        Button("Create Table", id="create"),
+                        INSERT,
+                        SELECT,
+                        CREATE,
                         id="top-left",
                     ),
                     Container(
-                        # Static("Enter vegetable:", id="request-text"),
-                        Input("Enter vegetable", id="vegetable"),
-                        # Static("Enter quantity: ", id="request-text"),
-                        Input("Enter quantity", id="quantity"),
+                        Input(placeholder="Enter vegetable", id="vegetable"),
+                        Input(placeholder="Enter quantity", id="quantity"),
                         Container(
-                            Static("Error", id="one"),
-                            Static(input_error, id="int-error"),
-                            Static("Enter additional information", id="two"),
-                            Static("Everything looks good", id="three"),
+                            ERROR,
+                            WARNING,
+                            CONFIRM,
                             id="middle-pane",
                         ),
                         id="top-right",
@@ -46,7 +60,7 @@ class TuiGrid(App):
                     id="bottom-pane",
                     ),
                     Vertical(
-                        Button("Submit  ", id="box1"),
+                        SUBMIT,
                         Static("", id="box2"),
                         Static("", id="box3"),
                         id="right-pane",
@@ -58,32 +72,83 @@ class TuiGrid(App):
                     id="footer",
                 ),
                 )
+                
+    def on_mount(self):
+        ERROR.display = False
+        WARNING.display = False
+        CONFIRM.display = False
 
-class HandleButton(Static):
+    def animate_button(self) -> None:
+        SUBMIT.styles.offset = (1, int(i[0]))
 
-    class Selected(Message):
 
-        def __init__(self, sender: MessageTarget, offset: Button.offset) -> None:
-            self.offset = offset
-            super().__init__(sender)
-    
-    def __init__(self, offset: Button.offset) -> None:
-        self.offset = offset
-        super().__init__()
+    def on_button_pressed(self, event: Button.Pressed) -> None:
 
-    async def on_click(self) -> None:
-        await self.emit(self.Selected(self, self.offset))   
-    
+        button_id = event.button.id
 
-class HandlePull(App):
-    def compose(self) -> ComposeResult:
-        yield HandleButton(Button.offset.parse("1,1"))
+        if button_id == "create":
+            # setup()
+            if CONFIRM.display == True:
+                CONFIRM.display = False
+            elif CONFIRM.display == False:
+                if ERROR.display == True:
+                    ERROR.display = False
+                if WARNING.display == True:
+                    WARNING.display = False
+                CONFIRM.display = True
+ 
+        elif button_id == "insert":
+            # if self.query(Input.id) == "vegetable":
+            #     name = Input.value
+            # if self.query(Input.id) == "quantity":
+            #     quantity = Input.value
+            # insert_vegetable(name, quantity)
+            if WARNING.display == True:
+                WARNING.display = False
+            elif WARNING.display == False:
+                if CONFIRM.display == True:
+                    CONFIRM.display = False
+                if ERROR.display == True:
+                    ERROR.display = False
+                WARNING.display = True
 
-    def on_handel_click(self, message: HandleButton.Selected) -> None:
-        self.screen.styles.animate("offset", message.handle, duration=0.5)
+        elif button_id == "select":
+            # if self.query(Input.id) == "vegetable":
+            #     name = Input.value
+            # if self.query(Input.id) == "quantity":
+            #     quantity = Input.value
+            # select_vegetable(name, quantity)
+            if ERROR.display == True:
+                ERROR.display = False
+            elif ERROR.display == False:
+                if CONFIRM.display == True:
+                    CONFIRM.display = False
+                if WARNING.display == True:
+                    WARNING.display = False
+                ERROR.display = True
+
+        elif button_id == "submit":
+            if i[0] in range(0, 15):
+                time.sleep(0.1)
+                while i[0] in range(0, 15):
+                    i[0] = i[0] + 1
+                    SUBMIT.styles.offset = (1, int(i[0]))
+            elif i[0] == 15:
+                i[0] = 14
+                time.sleep(0.1)
+                while 1 <= i[0] <= 14:
+                    i[0] = i[0] - 1
+                    SUBMIT.styles.offset = (1, int(i[0]))
+            if 0 > i[0] > 15:
+                i.clear()
+                i.append(0)
+
+
+
+            
 
 if __name__ == "__main__":
-    app = TuiGrid(css_path="grid0.css")
+    app = TuiGrid(css_path="grid.css")
     app.run()
 
 
